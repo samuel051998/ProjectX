ab7b8e5c65a5eaac405a14907ae31da38ae174a5
samuele <samuele@amasoftware.com>
2020-09-29 11:38:07 +0200

    progetto tabs, progetto minimal(planets)

---
+++ testplanetflutter/android/app/src/main/kotlin/com/example/testplanetflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testplanetflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ testplanetflutter/lib/homepage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import './constants.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: gradientEndColor,
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Explore',
+                      style: TextStyle(
+                        fontFamily: 'Avenir',
+                        fontSize: 44,
+                        color: const Color(0xffffffff),
+                        fontWeight: FontWeight.w900,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    DropdownButton(
+                      items: [
+                        DropdownMenuItem(
+                          child: Text(
+                            'Solar System',
+                            style: TextStyle(
+                              fontFamily: 'Avenir',
+                              fontSize: 24,
+                              color: const Color(0x7cdbf1ff),
+                              fontWeight: FontWeight.w500,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        )
+                      ],
+                      onChanged: (value) {},
+                      icon: Image.asset('assets/drop_down_icon.png'),
+                      underline: SizedBox(),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 500,
+                child: Swiper(
+                  itemCount: ,
+                ),
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
