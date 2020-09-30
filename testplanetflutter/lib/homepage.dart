65fb448a875554aedd778e6f6bd9984e22c697d6
samuele <samuele@amasoftware.com>
2020-09-30 10:11:19 +0200

    continuazione test planets

--- testplanetflutter/lib/homepage.dart
+++ testplanetflutter/lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import './constants.dart';
+import './data.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -46,7 +48,7 @@ class _HomePageState extends State<HomePage> {
                         )
                       ],
                       onChanged: (value) {},
-                      icon: Image.asset('assets/drop_down_icon.png'),
+                      icon: Image.asset('./assets/drop_down_icon.png'),
                       underline: SizedBox(),
                     ),
                   ],
@@ -54,8 +56,78 @@ class _HomePageState extends State<HomePage> {
               ),
               Container(
                 height: 500,
+                padding: const EdgeInsets.only(left: 32),
                 child: Swiper(
-                  itemCount: ,
+                  itemCount: planets.length,
+                  itemWidth: MediaQuery.of(context).size.width - 2 * 64,
+                  layout: SwiperLayout.STACK,
+                  itemBuilder: (context, index) {
+                    return Stack(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            SizedBox(height: 100),
+                            Card(
+                              elevation: 10,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(32),
+                              ),
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.all(42.0),
+                                child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  SizedBox(height: 100),
+                                  Text(
+                                    planets[index].name,
+                                    style: TextStyle(
+                                      fontFamily: 'Avenir',
+                                      fontSize: 44,
+                                      color: const Color(0xff47455f),
+                                      fontWeight: FontWeight.w900,
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  Text(
+                                    'Solar System',
+                                    style: TextStyle(
+                                      fontFamily: 'Avenir',
+                                      fontSize: 23,
+                                      color: primaryTextColor,
+                                      fontWeight: FontWeight.w500,        
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  SizedBox(height: 32),
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        'Know more',
+                                        style: TextStyle(
+                                          fontFamily: 'Avenir',
+                                          fontSize: 18,
+                                          color: secondaryTextColor,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      Icon(
+                                        Icons.arrow_forward_ios,
+                                        color: secondaryTextColor,
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              )
+                            )
+                          ],
+                        ),
+                        Image.asset(planets[index].iconImage)
+                      ],
+                    );
+                  },
                 ),
               )
             ],
