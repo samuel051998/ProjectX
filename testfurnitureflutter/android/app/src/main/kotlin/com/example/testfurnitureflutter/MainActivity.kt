212ed34b0027a549b4b5860d9d30f60531c74016
samuele <samuele@amasoftware.com>
2020-10-05 12:33:08 +0200

    furnitureTestProject

---
+++ testfurnitureflutter/android/app/src/main/kotlin/com/example/testfurnitureflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testfurnitureflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ testfurnitureflutter/lib/screens/product/products_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:testfurnitureflutter/constants.dart';
+
+class ProductsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+    );
+  }
+}
