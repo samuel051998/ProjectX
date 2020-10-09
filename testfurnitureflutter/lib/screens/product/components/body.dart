762dad031f87beb87e06ae8c8a9113de99a6ac77
samuele <samuele@amasoftware.com>
2020-10-09 10:50:17 +0200

    aggiunta product card e aggiunta pagina details_screen

--- testfurnitureflutter/lib/screens/product/components/body.dart
+++ testfurnitureflutter/lib/screens/product/components/body.dart
@@ -1,86 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:testfurnitureflutter/modals/product.dart';
 import '../../../components/search_box.dart';
 import '../../../constants.dart';
 import 'category_list.dart';
+import 'product_card.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        SearchBox(),
-        Categorylist(),
-        SizedBox(height: kDefaultPadding / 2),
-        Expanded(
-            child: Stack(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(top: 70),
-              decoration: BoxDecoration(
-                color: kBackgroundColor,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40)),
-              ),
-            ),
-            ProductCard(),
-          ],
-        ))
-      ],
-    );
-  }
-}
-
-class ProductCard extends StatelessWidget {
-  const ProductCard({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(
-          horizontal: kDefaultPadding, vertical: kDefaultPadding / 2),
-      // color: Colors.blueAccent,
-      height: 160,
-      child: Stack(
-        alignment: Alignment.bottomCenter,
+    return SafeArea(
+      bottom: false, 
+      child: Column(
         children: <Widget>[
-          Container(
-            height: 136,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(22),
-              color: kBlueColor,
-              boxShadow: [kDefaultShadow],
-            ),
-            child: Container(
-              margin: EdgeInsets.only(right: 10),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(22)),
-            ),
-          ),
-          Positioned(
-            top: 0,
-            right: 0,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 160,
-              width: 200,
-              child: Image.asset(
-                'assets/images/Item_1.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Positioned(
-            child: SizedBox(
-              height: 136,
-              width: size.width - 200,
-            ),
-          ),
+          SearchBox(onChanged: (value) {}),
+          Categorylist(),
+          SizedBox(height: kDefaultPadding / 2),
+          Expanded(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 70),
+                  decoration: BoxDecoration(
+                    color: kBackgroundColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40)
+                    ),
+                  ),
+                ),
+                ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) => ProductCard(
+                    itemIndex: index,
+                    product: products[index],
+                    press: () {},
+                  ),
+                )
+              ],
+            )
+          )
         ],
       ),
     );
   }
 }
+
