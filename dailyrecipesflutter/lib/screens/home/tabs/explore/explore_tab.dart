624b19a32e49644eb92229e6a5e76a220c435247
samuele <samuele@amasoftware.com>
2020-12-14 09:52:22 +0100

    Add searchbar in explore tab and add searchbar dependencies

--- dailyrecipesflutter/lib/screens/home/tabs/explore/explore_tab.dart
+++ dailyrecipesflutter/lib/screens/home/tabs/explore/explore_tab.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'components/body.dart';
+
 class ExploreTab extends StatelessWidget {
   static String routeName = '/explore';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Body(),
     );
   }
 }
